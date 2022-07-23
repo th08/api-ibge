34e3b4b33fa9c64005a4d7b790f0160c74cf27ad
th08 <thiagomiranda0075@gmail.com>
2022-07-23 15:39:29 -0300

    implementação final do projeto

--- resources/views/layouts/_inc/form-edit.blade.php
+++ resources/views/layouts/_inc/form-edit.blade.php
@@ -1,46 +1,40 @@
 @csrf
-    @if(isset($registro->cidade))
-        {{$registro}}
-        <script>
-            let userCity = $registro->cidade;
-        </script>
-    @endif
-        <div class="row">
-            <div class="col-6">
+<div class="row">
+    <div class="col-6">
 
-                <div class="form-group">
-                    <label for="">Nome</label>
-                    <input type="text" name="nome" id="" class="form-control" value="{{isset($registro->nome) ? $registro->nome:old('nome')}}">
-                </div>
-
-            </div>
-            <div class="col-6">
-
-                <div class="form-group">
-                    <label for="">Email</label>
-                    <input type="email" name="email" id="" class="form-control" value="{{isset($registro->email) ? $registro->email:old('email')}}">
-                </div>
-
-            </div>
+        <div class="form-group">
+            <label for="">Nome</label>
+            <input type="text" name="nome" id="" class="form-control" value="{{isset($registro->nome) ? $registro->nome:old('nome')}}">
         </div>
 
-        <div class="row">
-            <div class="col-6">
-                <div class="form-group">
-                    <label for="">Selecione seu estado</label>
-                    <select name="estado_id" id="select-state" class="form-control">
-                        @foreach ($estados as $estado)                                             
-                        <option value="{{isset($estado->id ) ? $estado->id : old('estado_id')}}" {{$estado->id === $registro->estado_id ? "selected" : ''}}>{{isset($estado->nomeEstado) ? $estado->nomeEstado : ''}}</option>                        
-                        @endforeach                      
-                    </select>
-                </div>
-            </div>
-            <div class="col-6">
-                <div class="form-group">
-                    <label for="">Selecione sua cidade</label>
-                    <select name="cidade" id="select-city" class="form-control">
-                        <option value="{{!isset($registro->cidade) ? old('cidade') : $registro->cidade }}">{{!isset($registro->cidade) ? old('cidade') : $registro->cidade }}</option>
-                    </select>
-                </div>
-            </div>
+    </div>
+    <div class="col-6">
+
+        <div class="form-group">
+            <label for="">Email</label>
+            <input type="email" name="email" id="" class="form-control" value="{{isset($registro->email) ? $registro->email:old('email')}}">
         </div>
+
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-6">
+        <div class="form-group">
+            <label for="">Selecione seu estado</label>
+            <select name="estado_id" id="select-state" class="form-control">
+                @foreach ($estados as $estado)                                             
+                <option value="{{isset($estado->id ) ? $estado->id : old('estado_id')}}" {{$estado->id === $registro->estado_id ? "selected" : ''}}>{{isset($estado->nomeEstado) ? $estado->nomeEstado : ''}}</option>                        
+                @endforeach                      
+            </select>
+        </div>
+    </div>
+    <div class="col-6">
+        <div class="form-group">
+            <label for="">Selecione sua cidade</label>
+            <select name="cidade" id="select-city" class="form-control">
+                <option value="{{!isset($registro->cidade) ? old('cidade') : $registro->cidade }}">{{!isset($registro->cidade) ? old('cidade') : $registro->cidade }}</option>
+            </select>
+        </div>
+    </div>
+</div>
